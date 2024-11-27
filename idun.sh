403cf98bc6c584f9852edba5b67a3bb76cf5f0ff
Andreas Winje <andrebw@stud.ntnu.no>
2024-11-27 15:30:00 +0100

    updated slurm script

--- idun.sh
+++ idun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #SBATCH --partition=GPUQ
-#SBATCH --job-name=v5/SwinUNETR
+#SBATCH --job-name=v5/UNet
 #SBATCH --account=share-ie-idi
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -14,6 +14,12 @@
 #SBATCH --error=logs/%x/idun/out-%j.log
 #SBATCH --nodelist=idun-06-[01-07]
 
+# Get model type from IDUN Job Name
+export MODEL=$(echo $SLURM_JOB_NAME | cut -d"/" -f 2)
+export CHECKPOINT_STEP=23100  # set to load specific checkpoint
+
+echo Model: $MODEL
+
 echo $SLURM_JOB_NAME-$SLURM_JOB_ID
 cd /cluster/work/andrebw/medical-image-segmentation
 pwd
@@ -23,7 +29,7 @@ nvidia-smi
 
 python run_training.py \
   --experiment-id $SLURM_JOB_NAME  \
-  --model-type SwinUNETR \
+  --model-type $MODEL \
   --batch-size 4  \
   --epochs 700  \
   --lr-init 1e-4  \
@@ -32,5 +38,5 @@ python run_training.py \
   --smooth-nr 0  \
   --smooth-dr 1e-5  \
   --cpu-cores $SLURM_CPUS_ON_NODE \
-#  --no-training
-#  --checkpoint "logs/v2/HNTS-UNETR/checkpoints/16500" \
+#  --no-training \
+#  --checkpoint "logs/$SLURM_JOB_NAME/checkpoints/$CHECKPOINT_STEP" \
